d9d9047155bac32e1078193673e4e8a584e0b532
slashd0t <d3v0ps00001@gmail.com>
2026-02-11 02:28:49 +0800

    Fix the CORS missing allow origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,25 +7,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    'http://localhost:3000',
-    process.env.CLIENT_ORIGIN,
-  ].filter(Boolean);
+  const allowedOrigins = [process.env.CLIENT_ORIGIN ?? 'http://localhost:3000'];
 
   app.enableCors({
-    // origin: (origin, callback) => {
-    //   // allow server-to-server & tools like Postman
-    //   if (!origin) return callback(null, true);
-
-    //   if (allowedOrigins.includes(origin)) {
-    //     return callback(null, true);
-    //   }
-
-    //   return callback(new Error(`CORS blocked origin: ${origin}`), false);
-    // },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    origin: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 

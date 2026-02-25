36b4240fc68f413bdb09a92464663c0f36390138
slashd0t <d3v0ps00001@gmail.com>
2026-02-26 01:50:02 +0800

    Remove dist folder

--- apps/api/dist/posts/dto/update-post.dto.d.ts
+++
@@ -1,4 +0,0 @@
-declare const UpdatePostDto_base: any;
-export declare class UpdatePostDto extends UpdatePostDto_base {
-}
-export {};
--- apps/api/dist/posts/post.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Post {
-    id: string;
-    title: string;
-    content: string;
-    author: string;
-    published: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-}
